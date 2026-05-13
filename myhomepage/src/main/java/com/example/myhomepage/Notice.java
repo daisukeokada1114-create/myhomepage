ddf1d417f9d6a7073a4395d13bf29b7f170fdb0f
daisukeokada1114-create <daisuke.okada1114@gmail.com>
2026-05-13 15:36:46 +0900

    レスポンシブ対応とNoticeテーブル作成

---
+++ myhomepage/src/main/java/com/example/myhomepage/Notice.java
@@ -0,0 +1,47 @@
+package com.example.myhomepage;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+// このクラスをDBテーブルとして扱う
+@Entity
+public class Notice {
+
+  // 主キー
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  // お知らせタイトル
+  private String title;
+
+  // お知らせ本文
+  private String content;
+
+  // ID取得
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  // タイトル取得
+  public String getTItle() {
+    return title;
+  }
+
+  // タイトル設定
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  // 本文取得
+  public String getContent() {
+    return content;
+  }
+
+  // 本文設定
+  public void setContent(String content) {
+    this.content = content;
+  }
+}
